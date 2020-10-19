54d7e8761dff5d3378a86deefe031323db48ae19
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-19 17:20:01 -0300

    fix: Fix useCase

--- functions/src/core/UseCase.ts
+++ functions/src/core/UseCase.ts
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/no-extraneous-class */
 import { Failure } from './Failure'
 
 export interface UseCase<T> {
-  execute: (p: unknown) => Promise<T | Failure>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  execute: (p: any) => Promise<T | Failure>
 }
