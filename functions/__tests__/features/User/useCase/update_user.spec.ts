6bfd64cc61255427ccc9c6778fa800af12a07b14
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-19 18:05:49 -0300

    test: update user test

---
+++ functions/__tests__/features/User/useCase/update_user.spec.ts
@@ -0,0 +1,27 @@
+import { User } from "../../../../src/model/User";
+import { UseCase } from '../../../../src/core/UseCase';
+import { UpdateUser } from "../../../../src/features/User/useCase/UpdateUser";
+import { UserRepository } from '../../../../src/features/User/repository/UserRepository';
+import { Failure } from '../../../../src/core/Failure';
+
+
+describe('update user address and prefferences', () => {
+        const userRepositoryStub: UserRepository = {
+            createUser: (user: User): Promise<User | Failure> => { return (Promise.resolve(user)) },
+            updateUser: (user: User): Promise<User | Failure> => { 
+                const response = {...user}; 
+                response.address = user.address;
+                response.preferences = user.preferences;
+                return Promise.resolve(response);
+            }
+        }
+
+    it('update user preferences', async () => {
+        const sut: UseCase<User> = new UpdateUser(userRepositoryStub);
+        const expected: User = {email:'email', userId: '1', name: "Name", preferences:{searchRadius: 10000}};
+        const user = {...expected}
+        const actual = await sut.execute(user);
+        expect(actual).toEqual(user);
+    });
+
+});
