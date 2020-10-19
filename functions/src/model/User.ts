e495a4249f03380ddd41cfe670beb1fa1b38f019
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-19 15:09:13 -0300

    feat: add UserPreferences and address to User

--- functions/src/model/User.ts
+++ functions/src/model/User.ts
@@ -1,6 +1,11 @@
+import { Address } from './Address'
+import { UserPreferences } from './UserPreferences';
+
 export interface User {
   userId: string
   name?: string
   email: string
   taxId?: string
+  address?: Address
+  preferences: UserPreferences
 }
