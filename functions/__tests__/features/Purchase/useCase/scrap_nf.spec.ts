d4663d42e6abdbaeef770438c5af33b583adf12e
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-19 18:06:27 -0300

    test: scrap nf test

---
+++ functions/__tests__/features/Purchase/useCase/scrap_nf.spec.ts
@@ -0,0 +1,42 @@
+import { HtmlFiscalNote } from "../../../../src/model/HtmlFiscalNote";
+import { Purchase } from "../../../../src/model/Purchase";
+import { ScrapNFUseCase } from "../../../../src/features/Purchase/useCase/ScrapNFUseCase"
+import { ScrapNFService } from "../../../../src/features/Purchase/service/ScrapNFService";
+
+describe('scrap NF html', () => {
+
+    const sample: Purchase = {
+        'fiscalNote': {
+            date: new Date(),
+            accessKey: "lsdfaslkjfls", 
+            number: '87687',
+            series: '98',
+            company: {
+                name: 'iuoi',
+                taxIdentification: '9879',
+                address: {
+                    rawAddress: 'kjhkj'
+                }
+            }
+        },
+        totalAmount: 10.5,
+        purchaseItemList: []
+    };
+
+    const scrapNfService: ScrapNFService = {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        scrapNf(htmlFiscalNote: HtmlFiscalNote){
+            return Promise.resolve(sample);
+        }
+    }
+    it('should scrap nf HTML and return a Purchase object', async () => {        
+        const htmlFiscalNote: HtmlFiscalNote = {html: 'html', uf: 'MG'};
+        const expected: Purchase = {...sample};
+        const sut = new ScrapNFUseCase(scrapNfService)
+        const actual = await sut.execute(htmlFiscalNote);
+        expect(actual).toEqual(expected);
+
+        
+    });
+    
+});
