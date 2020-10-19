32c2450263080e3d3c48169931888bac91ad9379
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-19 17:21:20 -0300

    test: createUser test

---
+++ functions/__tests__/features/User/useCase/create_user.spec.ts
@@ -0,0 +1,22 @@
+import { User } from "../../../../src/model/User";
+import { UseCase } from '../../../../src/core/UseCase';
+import { CreateUser } from "../../../../src/features/User/useCase/CreateUser";
+import { UserRepository } from '../../../../src/features/User/repository/UserRepository';
+import { Failure } from '../../../../src/core/Failure';
+
+
+describe('create user', () => {
+        const userRepositoryStub: UserRepository = {
+            createuser : (user: User):Promise<User|Failure> =>  {return( Promise.resolve(user))}
+        }
+
+    it('should call create user on repository and return status', async () => {
+        const sut: UseCase<User> = new CreateUser(userRepositoryStub);
+        const user: User = {email:'email', userId: '1', name: "Name", preferences:{searchRadius: 10000}};
+        const actual = await sut.execute(user);
+        expect(actual).toEqual(user);
+
+
+    });
+
+});
