0ba384e94c870e027c9ec8ce8f181510af8f3ad4
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-21 08:58:51 -0300

    refactor: reformat code

--- functions/src/services/ElasticSearchServices.ts
+++ functions/src/services/ElasticSearchServices.ts
@@ -1,38 +1,57 @@
-import { ElasticSearchDocument } from '../model/ElasticSearchDocument'
-import { ElasticSearchRepository } from '../repository/ElasticSearchRepository'
+import { ElasticSearchDocument } from "../model/ElasticSearchDocument";
+import { ElasticSearchRepository } from "../repository/ElasticSearchRepository";
 
 export abstract class ElasticSearchService {
-  protected _repository: ElasticSearchRepository
-  protected constructor (_repository: ElasticSearchRepository) {
-    this._repository = _repository
+  protected _repository: ElasticSearchRepository;
+  protected constructor(_repository: ElasticSearchRepository) {
+    this._repository = _repository;
   }
 
-  abstract insertIntoElasticSearch (elasticSearchDocument: ElasticSearchDocument): Promise<void>
-  abstract getAutocompleteByPhrasePrefix (prefix: string): Promise<ElasticSearchDocument[]>
+  abstract insertIntoElasticSearch(
+    elasticSearchDocument: ElasticSearchDocument
+  ): Promise<void>;
+  abstract getAutocompleteByPhrasePrefix(
+    prefix: string
+  ): Promise<ElasticSearchDocument[]>;
 }
 
 export class ElasticSearchServiceImplementation extends ElasticSearchService {
-  private readonly index = 'produtos_autocomplete'
+  private readonly index = "produtos_autocomplete";
 
-  private static instance: ElasticSearchService
-  private constructor (_repository: ElasticSearchRepository) {
-    super(_repository)
+  private static instance: ElasticSearchService;
+  private constructor(_repository: ElasticSearchRepository) {
+    super(_repository);
   }
 
-  public static getInstance (_repository: ElasticSearchRepository): void {
+  public static getInstance(
+    _repository: ElasticSearchRepository
+  ): ElasticSearchService {
     if (!this.instance) {
-      this.instance = new ElasticSearchServiceImplementation(_repository)
+      this.instance = new ElasticSearchServiceImplementation(_repository);
     }
-    return this.instance
+    return this.instance;
   }
 
-  async insertIntoElasticSearch (elasticSearchDocument: ElasticSearchDocument): Promise<void> {
-    const doc = elasticSearchDocument.firebaseDocId
-    await this._repository.insertIntoElasticSearch(this.index, doc, elasticSearchDocument)
+  async insertIntoElasticSearch(
+    elasticSearchDocument: ElasticSearchDocument
+  ): Promise<void> {
+    const doc = elasticSearchDocument.firebaseDocId;
+    await this._repository.insertIntoElasticSearch(
+      this.index,
+      doc,
+      elasticSearchDocument
+    );
   }
 
-  async getAutocompleteByPhrasePrefix (prefix: string): Promise<ElasticSearchDocument[]> {
-    const response = await this._repository.getAutocompleteByPhrasePrefix(this.index, prefix)
-    return response.data.hits.hits.map((i: { _source: ElasticSearchDocument }) => i._source)
+  async getAutocompleteByPhrasePrefix(
+    prefix: string
+  ): Promise<ElasticSearchDocument[]> {
+    const response = await this._repository.getAutocompleteByPhrasePrefix(
+      this.index,
+      prefix
+    );
+    return response.data.hits.hits.map(
+      (i: { _source: ElasticSearchDocument }) => i._source
+    );
   }
 }
