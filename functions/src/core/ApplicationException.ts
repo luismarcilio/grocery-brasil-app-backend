8341a41206a283e637e3096ff5b8c6d09e50833b
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-22 16:18:31 -0300

    feat: purchaseRepositoryFirebase

--- functions/src/core/ApplicationException.ts
+++ functions/src/core/ApplicationException.ts
@@ -5,6 +5,7 @@ export interface ApplicationException {
 export enum MessageIds {
   UNEXPECTED,
   UNIMPLEMENTED,
+  INVALID_ARGUMENT,
 }
 
 export class ScrapNfException implements ApplicationException {
---
+++ functions/src/features/Purchase/data/PurchaseRepositoryFirebase.ts
@@ -0,0 +1,72 @@
+import { PurchaseRepository } from "./PurchaseRepository";
+import { Purchase, PurchaseResume } from "../../../model/Purchase";
+import {
+  PurchaseException,
+  MessageIds,
+} from "../../../core/ApplicationException";
+
+export class PurchaseRepositoryFirebase implements PurchaseRepository {
+  private readonly firestore: FirebaseFirestore.Firestore;
+  constructor(firestore: FirebaseFirestore.Firestore) {
+    this.firestore = firestore;
+  }
+  savePurchase = async (purchase: Purchase): Promise<boolean> => {
+    const userId = purchase.user?.userId;
+    if (!userId) {
+      throw new PurchaseException({
+        messageId: MessageIds.INVALID_ARGUMENT,
+        message: "undefined userId",
+      });
+    }
+    const purchaseId = purchase.fiscalNote.accessKey;
+    if (!purchaseId) {
+      throw new PurchaseException({
+        messageId: MessageIds.INVALID_ARGUMENT,
+        message: "undefined purchaseId",
+      });
+    }
+    try {
+      const userDocRef = this.firestore.collection("COMPRAS").doc(userId);
+      await userDocRef.set({ userDocId: userId });
+      const dbNfDoc = this.firestore
+        .collection("COMPRAS")
+        .doc(userId)
+        .collection("COMPLETA")
+        .doc(purchaseId);
+
+      dbNfDoc.set(purchase);
+      return true;
+    } catch (error) {
+      throw new PurchaseException({
+        messageId: MessageIds.UNEXPECTED,
+        message: error,
+      });
+    }
+  };
+  savePurchaseResume = async (
+    purchaseResume: PurchaseResume
+  ): Promise<boolean> => {
+    const userId = purchaseResume.user?.userId;
+    if (!userId) {
+      throw new PurchaseException({
+        messageId: MessageIds.INVALID_ARGUMENT,
+        message: "undefined userId",
+      });
+    }
+
+    try {
+      const dbNfDoc = this.firestore
+        .collection("COMPRAS")
+        .doc(userId)
+        .collection("RESUMIDA")
+        .doc(purchaseResume.accessKey);
+      await dbNfDoc?.set(purchaseResume);
+      return Promise.resolve(true);
+    } catch (error) {
+      throw new PurchaseException({
+        messageId: MessageIds.UNEXPECTED,
+        message: error,
+      });
+    }
+  };
+}
