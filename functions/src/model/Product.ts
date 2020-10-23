9a9aa05486d5c33630867ae9bef7c6910399e960
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-23 10:12:40 -0300

    fix: added accessKey to productPurchase

--- functions/src/model/Product.ts
+++ functions/src/model/Product.ts
@@ -1,17 +1,18 @@
-import { Unity } from './Unity'
-import { Company } from './Company'
+import { Unity } from "./Unity";
+import { Company } from "./Company";
 export interface Product {
-  name: string
-  eanCode: string
-  ncmCode: string
-  unity: Unity
-  normalized?: boolean
-  thumbnail?: string
-  normalizationStatus?: number
+  name: string;
+  eanCode: string;
+  ncmCode: string;
+  unity: Unity;
+  normalized?: boolean;
+  thumbnail?: string;
+  normalizationStatus?: number;
 }
 
 export interface ProductPurchase {
-  company: Company
-  date: Date
-  unityValue: number
+  accessKey: string;
+  company: Company;
+  date: Date;
+  unityValue: number;
 }
