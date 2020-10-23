dd800f0c63fc9185da201f5be6710cd08c9ccf37
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-23 10:14:47 -0300

    test: ProductServiceImpl

---
+++ functions/__tests__/features/Product/fixtures.ts
@@ -0,0 +1,8 @@
+import { Product } from "../../../src/model/Product";
+
+export const product: Product = {
+  name: "product",
+  eanCode: "eanCode",
+  ncmCode: "ncmCode",
+  unity: { name: "un" },
+};
---
+++ functions/__tests__/features/Product/service/ProductServiceImpl.spec.ts
@@ -0,0 +1,133 @@
+import { ProductService } from "../../../../src/features/Product/service/ProductService";
+import { ProductProvider } from "../../../../src/features/Product/provider/ProductProvider";
+import { purchase } from "../../Purchase/fixtures/purchases";
+import { ProductServiceImpl } from "../../../../src/features/Product/service/ProductServiceImpl";
+import { ProductPurchase } from "../../../../src/model/Product";
+import {
+  ProductException,
+  MessageIds,
+} from "../../../../src/core/ApplicationException";
+
+describe("ProductService implementation", () => {
+  const save = jest.fn();
+  const saveNf = jest.fn();
+  const getProductById = jest.fn();
+  const getDocId = jest.fn();
+  const productProviderStub: ProductProvider = {
+    save,
+    saveNf,
+    getProductById,
+    getDocId,
+  };
+  const sut: ProductService = new ProductServiceImpl(productProviderStub);
+
+  it("should save each product", async () => {
+    jest.resetAllMocks();
+    jest.spyOn(productProviderStub, "save").mockResolvedValue(true);
+    jest.spyOn(productProviderStub, "saveNf").mockResolvedValue(true);
+    jest.spyOn(productProviderStub, "getDocId").mockReturnValueOnce("00001");
+    jest
+      .spyOn(productProviderStub, "getDocId")
+      .mockReturnValueOnce("00002-product-2");
+
+    jest
+      .spyOn(productProviderStub, "getProductById")
+      .mockResolvedValue(undefined);
+
+    await sut.saveItemsFromPurchase(purchase);
+    expect(save).toHaveBeenNthCalledWith(1, {
+      eanCode: "00001",
+      name: "product1",
+      ncmCode: "00001",
+      unity: { name: "UN" },
+    });
+    expect(save).toHaveBeenNthCalledWith(2, {
+      eanCode: "",
+      name: "product/2",
+      ncmCode: "00002",
+      unity: { name: "UN" },
+    });
+  });
+  it("should check if the product exists before save", async () => {
+    jest.resetAllMocks();
+    jest.spyOn(productProviderStub, "save").mockResolvedValue(true);
+    jest.spyOn(productProviderStub, "saveNf").mockResolvedValue(true);
+
+    jest
+      .spyOn(productProviderStub, "getProductById")
+      .mockResolvedValue(undefined);
+    jest
+      .spyOn(productProviderStub, "getProductById")
+      .mockResolvedValueOnce(purchase.purchaseItemList[0].product);
+    jest.spyOn(productProviderStub, "getDocId").mockReturnValueOnce("00001");
+    jest
+      .spyOn(productProviderStub, "getDocId")
+      .mockReturnValueOnce("00002-PRODUCT-2");
+    await sut.saveItemsFromPurchase(purchase);
+
+    expect(getProductById).toHaveBeenNthCalledWith(1, "00001");
+    expect(getProductById).toHaveBeenNthCalledWith(2, "00002-PRODUCT-2");
+    expect(save).toHaveBeenNthCalledWith(1, {
+      eanCode: "",
+      name: "product/2",
+      ncmCode: "00002",
+      unity: { name: "UN" },
+    });
+  });
+  it("should save the company, price and date of the purchase", async () => {
+    jest.resetAllMocks();
+    jest.spyOn(productProviderStub, "save").mockResolvedValue(true);
+    jest.spyOn(productProviderStub, "saveNf").mockResolvedValue(true);
+    jest.spyOn(productProviderStub, "getDocId").mockReturnValueOnce("00001");
+    jest
+      .spyOn(productProviderStub, "getDocId")
+      .mockReturnValueOnce("00002-product-2");
+
+    jest
+      .spyOn(productProviderStub, "getProductById")
+      .mockResolvedValue(undefined);
+
+    const productPurchase: ProductPurchase[] = [
+      {
+        accessKey: purchase.fiscalNote.accessKey,
+        company: purchase.fiscalNote.company,
+        date: purchase.fiscalNote.date,
+        unityValue: purchase.purchaseItemList[0].unityValue,
+      },
+      {
+        accessKey: purchase.fiscalNote.accessKey,
+        company: purchase.fiscalNote.company,
+        date: purchase.fiscalNote.date,
+        unityValue: purchase.purchaseItemList[1].unityValue,
+      },
+    ];
+    await sut.saveItemsFromPurchase(purchase);
+    expect(saveNf).toHaveBeenNthCalledWith(
+      1,
+      purchase.purchaseItemList[0].product,
+      productPurchase[0]
+    );
+    expect(saveNf).toHaveBeenNthCalledWith(
+      2,
+      purchase.purchaseItemList[1].product,
+      productPurchase[1]
+    );
+  });
+  it("should return ProductException if an exception occurs", async () => {
+    jest.resetAllMocks();
+    jest.spyOn(productProviderStub, "save").mockResolvedValue(true);
+    jest.spyOn(productProviderStub, "saveNf").mockRejectedValue("Error");
+    jest.spyOn(productProviderStub, "getDocId").mockReturnValue("DocId");
+
+    jest
+      .spyOn(productProviderStub, "getProductById")
+      .mockResolvedValue(undefined);
+    const actual = await sut.saveItemsFromPurchase(purchase);
+    expect(actual).toEqual(
+      new ProductException({
+        messageId: MessageIds.UNEXPECTED,
+        message: "Error",
+      })
+    );
+  });
+});
