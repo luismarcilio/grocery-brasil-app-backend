8dc738cb7a8cf59d73b8900a5abe61f41c6fabe3
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-27 14:58:39 -0300

    feat: Authentication middleware

---
+++ functions/__tests__/presentation/AuthenticationMiddleware.spec.ts
@@ -0,0 +1,75 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { User } from "../../src/model/User";
+import { UseCase } from "../../src/core/UseCase";
+import { Middleware } from "../../src/protocol/Middleware";
+import { HttpRequest } from "../../src/protocol/HttpProtocol";
+import { UserException, MessageIds } from "../../src/core/ApplicationException";
+import { AuthenticationMiddleware } from "../../src/presentation/AuthenticationMiddleware";
+
+describe("AuthenticationMiddleware", () => {
+  const execute = jest.fn();
+  const getUserByJWTUseCase: UseCase<User> = { execute };
+  const sut: Middleware = new AuthenticationMiddleware(getUserByJWTUseCase);
+  const someUser: User = {
+    userId: "userId",
+    email: "user@email",
+    address: { rawAddress: "some address" },
+    preferences: { searchRadius: 100 },
+  };
+
+  it("should insert user in the body and call next if all is ok", async () => {
+    const request: HttpRequest = {
+      body: {},
+      headers: { Authorization: "Bearer jwt" },
+    };
+    execute.mockResolvedValue(someUser);
+    const response = await sut.handle(request);
+    expect(response.status).toEqual(200);
+    expect(response.body.user).toEqual(someUser);
+    expect(execute).toHaveBeenCalledWith("jwt");
+  });
+  it("should return 401 if authorization header doesn't exist", async () => {
+    const request: HttpRequest = {
+      body: {},
+    };
+    execute.mockResolvedValue(someUser);
+    const response = await sut.handle(request);
+    expect(response.body).toEqual("Unauthorized");
+    expect(response.status).toEqual(401);
+  });
+  it("should return 401 if authorization isn't bearer", async () => {
+    const request: HttpRequest = {
+      headers: { Authorization: "XXXXX jwt" },
+      body: {},
+    };
+    execute.mockResolvedValue(someUser);
+    const response = await sut.handle(request);
+    expect(response.body).toEqual("Unauthorized");
+    expect(response.status).toEqual(401);
+  });
+  it("should return 401 if jwt token is invalid", async () => {
+    const request: HttpRequest = {
+      body: {},
+      headers: { Authorization: "Bearer jwt" },
+    };
+    execute.mockImplementation( (_) => Promise.resolve(
+      new UserException({ messageId: MessageIds.UNEXPECTED, message: "Erro" })
+    ));
+    const response = await sut.handle(request);
+    expect(response.status).toEqual(401);
+  });
+  it("should return 401 if user doesn't exist", async () => {
+    const request: HttpRequest = {
+      body: {},
+      headers: { Authorization: "Bearer jwt" },
+    };
+    execute.mockResolvedValue(
+      new UserException({
+        messageId: MessageIds.NOT_FOUND,
+        message: "User not found: jwt",
+      })
+    );
+    const response = await sut.handle(request);
+    expect(response.status).toEqual(401);
+  });
+});
---
+++ functions/src/presentation/AuthenticationMiddleware.ts
@@ -0,0 +1,42 @@
+import { Middleware } from "../protocol/Middleware";
+import { HttpRequest, HttpResponse } from "../protocol/HttpProtocol";
+import { User } from "../model/User";
+import { UseCase } from "../core/UseCase";
+
+export class AuthenticationMiddleware implements Middleware {
+  private readonly getUserByJWTUseCase: UseCase<User>;
+
+  constructor(getUserByJWTUseCase: UseCase<User>) {
+    this.getUserByJWTUseCase = getUserByJWTUseCase;
+  }
+
+  handle = async (request: HttpRequest): Promise<HttpResponse> => {
+    const jwt = request.headers?.Authorization?.split("Bearer ")[1];
+    if (jwt === undefined) {
+      const response: HttpResponse = {
+        body: "Unauthorized",
+        status: 401,
+      };
+
+      return Promise.resolve(response);
+    }
+
+    const result = await this.getUserByJWTUseCase.execute(jwt);
+
+    if (result.constructor.name === "UserException") {
+      const response: HttpResponse = {
+        body: "Unauthorized",
+        status: 401,
+      };
+
+      return Promise.resolve(response);
+    }
+    const response: HttpResponse = {
+      headers: request.headers,
+      body: request.body,
+      status: 200,
+    };
+    response.body.user = <User>result;
+    return response;
+  };
+}
