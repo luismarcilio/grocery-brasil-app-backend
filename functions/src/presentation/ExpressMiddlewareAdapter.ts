6310077ef9066ed68ae610bcad800d1e38c22c1c
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-27 14:58:49 -0300

    feat: express middleware adapter

---
+++ functions/src/presentation/ExpressMiddlewareAdapter.ts
@@ -0,0 +1,24 @@
+import { Middleware } from "../protocol/Middleware";
+import { HttpRequest } from "../protocol/HttpProtocol";
+import * as express from "express";
+
+export const expressMiddlewareAdapter = (middleware: Middleware) => {
+  return async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ): Promise<void> => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers,
+      body: req.body,
+    };
+
+    const httpResponse = await middleware.handle(httpRequest);
+    if (httpResponse.status === 200) {
+      req.body = httpResponse.body;
+      next();
+    }
+
+    res.status(httpResponse.status).send(httpResponse.body);
+  };
+};
