4123eed7dcf1b853c881cde2d8930a946a462e2b
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-27 14:58:23 -0300

    feat: add headers to HTTP protocol

--- functions/src/protocol/HttpProtocol.ts
+++ functions/src/protocol/HttpProtocol.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export interface HttpRequest {
   body?: any;
+  headers?: any;
 }
 
 export interface HttpResponse {
+  headers?: any;
   status: number;
   body?: any;
 }
