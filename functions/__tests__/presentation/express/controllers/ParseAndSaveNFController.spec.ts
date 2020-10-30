b14d6339347e34986f4c9d9bb421cc31bd3a626e
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-30 17:13:56 -0300

    feat: ParseAndSaveNFController

---
+++ functions/__tests__/presentation/express/controllers/ParseAndSaveNFController.spec.ts
@@ -0,0 +1,137 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Controller } from "../../../../src/presentation/express/controllers/Controller";
+import { HttpRequest, HttpResponse } from "../../../../src/core/HttpProtocol";
+import { purchase } from "../../../features/Purchase/fixtures/purchases";
+import { HtmlFiscalNote } from "../../../../src/model/HtmlFiscalNote";
+import {
+  MessageIds,
+  PurchaseException,
+} from "../../../../src/core/ApplicationException";
+import { ParseAndSaveNFController } from "../../../../src/presentation/express/controllers/ParseAndSaveNFController";
+import { ScrapNFUseCase } from "../../../../src/features/Purchase/useCase/ScrapNFUseCase";
+import { SavePurchaseUseCase } from "../../../../src/features/Purchase/useCase/SavePurchaseUseCase";
+import { ScrapNfException } from "../../../../src/core/ApplicationException";
+
+describe("ParseAndSaveNFController", () => {
+  const savePurchaseUseCaseExecute = jest.fn();
+  const scrapNFUseCaseExecute = jest.fn();
+  const savePurchaseUseCase: SavePurchaseUseCase = ({
+    execute: savePurchaseUseCaseExecute,
+  } as unknown) as SavePurchaseUseCase;
+  const scrapNFUseCase: ScrapNFUseCase = ({
+    execute: scrapNFUseCaseExecute,
+  } as unknown) as ScrapNFUseCase;
+
+  const parseAndSaveNFController: Controller = new ParseAndSaveNFController(
+    savePurchaseUseCase,
+    scrapNFUseCase
+  );
+
+  const request: HttpRequest = {
+    params: { state: "MG" },
+    body: { html: "html", user: purchase.user },
+  };
+
+  it("should call scrap NF", async () => {
+    scrapNFUseCaseExecute.mockResolvedValue(purchase);
+    savePurchaseUseCaseExecute.mockResolvedValue(true);
+    const expected: HttpResponse = {
+      status: 200,
+      body: { status: "SUCCESS" },
+    };
+    const expectedHtmlFiscalNote: HtmlFiscalNote = {
+      html: "html",
+      uf: "MG",
+    };
+
+    const actual = await parseAndSaveNFController.handle(request);
+    expect(actual).toEqual(expected);
+    expect(scrapNFUseCaseExecute).toBeCalledWith(expectedHtmlFiscalNote);
+    expect(savePurchaseUseCaseExecute).toBeCalledWith(purchase);
+  });
+  it("should return error 400 if scrap nf fails", async () => {
+    const expectedHtmlFiscalNote: HtmlFiscalNote = {
+      html: "html",
+      uf: "MG",
+    };
+
+    scrapNFUseCaseExecute.mockResolvedValue(
+      new ScrapNfException({
+        messageId: MessageIds.UNEXPECTED,
+        message: "error on html",
+      })
+    );
+    const expected: HttpResponse = {
+      status: 400,
+      body: { status: "error on html" },
+    };
+
+    const actual = await parseAndSaveNFController.handle(request);
+    expect(actual).toEqual(expected);
+    expect(scrapNFUseCaseExecute).toBeCalledWith(expectedHtmlFiscalNote);
+  });
+  it("should return error 500 if SavePurchaseUseCase fails", async () => {
+    scrapNFUseCaseExecute.mockResolvedValue(purchase);
+    savePurchaseUseCaseExecute.mockResolvedValue(
+      new PurchaseException({
+        messageId: MessageIds.UNEXPECTED,
+        message: "error on the database",
+      })
+    );
+    const expected: HttpResponse = {
+      status: 500,
+      body: { status: "error on the database" },
+    };
+    const expectedHtmlFiscalNote: HtmlFiscalNote = {
+      html: "html",
+      uf: "MG",
+    };
+
+    const actual = await parseAndSaveNFController.handle(request);
+    expect(actual).toEqual(expected);
+    expect(scrapNFUseCaseExecute).toBeCalledWith(expectedHtmlFiscalNote);
+    expect(savePurchaseUseCaseExecute).toBeCalledWith(purchase);
+  });
+  it("should return error 500 if an exception occurs", async () => {
+    scrapNFUseCaseExecute.mockImplementation((...args: any) => {
+      throw new Error("fail");
+    });
+    const expected: HttpResponse = {
+      status: 500,
+      body: { status: new Error("fail") },
+    };
+    const expectedHtmlFiscalNote: HtmlFiscalNote = {
+      html: "html",
+      uf: "MG",
+    };
+
+    const actual = await parseAndSaveNFController.handle(request);
+    expect(actual).toEqual(expected);
+    expect(scrapNFUseCaseExecute).toBeCalledWith(expectedHtmlFiscalNote);
+  });
+  it("should return error 400 if html is null or empty", async () => {
+    const thisRequest = { ...request };
+    thisRequest.body.html = "";
+    const expected: HttpResponse = {
+      status: 400,
+      body: { status: "Bad Request" },
+    };
+    const actual = await parseAndSaveNFController.handle(request);
+    expect(actual).toEqual(expected);
+  });
+  it("should throw error 500 if user is not present in the body", async () => {
+    jest.clearAllMocks();
+    const thisRequest = { ...request };
+    thisRequest.body.html = "html";
+    delete thisRequest.body.user;
+    scrapNFUseCaseExecute.mockResolvedValue(purchase);
+    savePurchaseUseCaseExecute.mockResolvedValue(true);
+    const expected: HttpResponse = {
+      status: 500,
+      body: { status: "user not found" },
+    };
+    const actual = await parseAndSaveNFController.handle(request);
+    expect(actual).toEqual(expected);
+  });
+});
---
+++ functions/src/presentation/express/controllers/ParseAndSaveNFController.ts
@@ -0,0 +1,79 @@
+import { Controller } from "./Controller";
+import { HttpRequest, HttpResponse } from "../../../core/HttpProtocol";
+import { Purchase } from "../../../model/Purchase";
+import { HtmlFiscalNote } from "../../../model/HtmlFiscalNote";
+import { SavePurchaseUseCase } from "../../../features/Purchase/useCase/SavePurchaseUseCase";
+import { ScrapNFUseCase } from "../../../features/Purchase/useCase/ScrapNFUseCase";
+import {
+  ScrapNfException,
+  PurchaseException,
+} from "../../../core/ApplicationException";
+
+export class ParseAndSaveNFController implements Controller {
+  private readonly savePurchaseUseCase: SavePurchaseUseCase;
+  private readonly scrapNFUseCase: ScrapNFUseCase;
+
+  constructor(
+    savePurchaseUseCase: SavePurchaseUseCase,
+    scrapNFUseCase: ScrapNFUseCase
+  ) {
+    this.savePurchaseUseCase = savePurchaseUseCase;
+    this.scrapNFUseCase = scrapNFUseCase;
+  }
+
+  handle = async (request: HttpRequest): Promise<HttpResponse> => {
+    try {
+      const htmlFiscalNote: HtmlFiscalNote = {
+        html: request.body?.html,
+        uf: request.params?.state,
+      };
+
+      if (!htmlFiscalNote.html || htmlFiscalNote.html.length === 0) {
+        const response: HttpResponse = {
+          status: 400,
+          body: { status: "Bad Request" },
+        };
+        return Promise.resolve(response);
+      }
+
+      const result = await this.scrapNFUseCase.execute(htmlFiscalNote);
+
+      if (result.constructor.name === "ScrapNfException") {
+        const response: HttpResponse = {
+          status: 400,
+          body: { status: (result as ScrapNfException).message },
+        };
+        return Promise.resolve(response);
+      }
+
+      const purchase: Purchase = result as Purchase;
+      if (!request.body.user) {
+        const response: HttpResponse = {
+          status: 500,
+          body: { status: "user not found" },
+        };
+        return Promise.resolve(response);
+      }
+      purchase.user = request.body.user;
+      const saveResult = await this.savePurchaseUseCase.execute(purchase);
+      if (saveResult.constructor.name === "PurchaseException") {
+        const response: HttpResponse = {
+          status: 500,
+          body: { status: (saveResult as PurchaseException).message },
+        };
+        return Promise.resolve(response);
+      }
+      const response: HttpResponse = {
+        status: 200,
+        body: { status: "SUCCESS" },
+      };
+      return response;
+    } catch (error) {
+      const response: HttpResponse = {
+        status: 500,
+        body: { status: error },
+      };
+      return Promise.resolve(response);
+    }
+  };
+}
