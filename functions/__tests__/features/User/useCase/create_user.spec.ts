a3d05c2995d60eef199916bf33b7a049f2ff8d03
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-30 14:52:58 -0300

    fix: required property and  required method

--- functions/__tests__/features/User/useCase/create_user.spec.ts
+++ functions/__tests__/features/User/useCase/create_user.spec.ts
@@ -2,24 +2,27 @@ import { User } from "../../../../src/model/User";
 import { UseCase } from "../../../../src/core/UseCase";
 import { CreateUser } from "../../../../src/features/User/useCase/CreateUser";
 import { UserRepository } from "../../../../src/features/User/repository/UserRepository";
-import { UserException } from "../../../../src/core/ApplicationException";
 
 describe("create user", () => {
+  const createUser = jest.fn();
+  const updateUser = jest.fn();
+  const validateJWT = jest.fn();
   const userRepositoryStub: UserRepository = {
-    createUser: (user: User): Promise<User | UserException> =>
-      Promise.resolve(user),
-    updateUser: (user: User): Promise<User | UserException> =>
-      Promise.resolve(user),
+    createUser,
+    updateUser,
+    validateJWT,
   };
 
   it("should call create user on repository and return status", async () => {
     const sut: UseCase<User> = new CreateUser(userRepositoryStub);
     const user: User = {
+      address: {rawAddress: ""},
       email: "email",
       userId: "1",
       name: "Name",
       preferences: { searchRadius: 10000 },
     };
+    createUser.mockReturnValue(user);
     const actual = await sut.execute(user);
     expect(actual).toEqual(user);
   });
--- functions/__tests__/features/User/useCase/update_user.spec.ts
+++ functions/__tests__/features/User/useCase/update_user.spec.ts
@@ -2,29 +2,27 @@ import { User } from "../../../../src/model/User";
 import { UseCase } from "../../../../src/core/UseCase";
 import { UpdateUser } from "../../../../src/features/User/useCase/UpdateUser";
 import { UserRepository } from "../../../../src/features/User/repository/UserRepository";
-import { UserException } from "../../../../src/core/ApplicationException";
 
 describe("update user address and prefferences", () => {
+  const createUser = jest.fn();
+  const updateUser = jest.fn();
+  const validateJWT = jest.fn();
   const userRepositoryStub: UserRepository = {
-    createUser: (user: User): Promise<User | UserException> => {
-      return Promise.resolve(user);
-    },
-    updateUser: (user: User): Promise<User | UserException> => {
-      const response = { ...user };
-      response.address = user.address;
-      response.preferences = user.preferences;
-      return Promise.resolve(response);
-    },
+    createUser,
+    updateUser,
+    validateJWT,
   };
 
   it("update user preferences", async () => {
     const sut: UseCase<User> = new UpdateUser(userRepositoryStub);
     const expected: User = {
+      address: { rawAddress: "" },
       email: "email",
       userId: "1",
       name: "Name",
       preferences: { searchRadius: 10000 },
     };
+    updateUser.mockResolvedValue(expected);
     const user = { ...expected };
     const actual = await sut.execute(user);
     expect(actual).toEqual(user);
