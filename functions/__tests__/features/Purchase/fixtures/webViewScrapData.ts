5e6f25ac2b405e2854591eff1bfda2c58f62c800
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-10-30 17:49:03 -0300

    feat: fixture

---
+++ functions/__tests__/features/Purchase/fixtures/webViewScrapData.ts
@@ -0,0 +1,8 @@
+import { WebViewScrapData } from "../../../../src/features/Purchase/model/WebViewScrapData";
+
+export const webViewScrapData: WebViewScrapData = {
+  initialUrl: "url",
+  uf: "MG",
+  accessKey: "0000000000",
+  javascriptFunctions: "kjhofhfhopfhopfhosdpophfd",
+};
