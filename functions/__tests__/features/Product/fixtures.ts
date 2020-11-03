db5924d4dd8974d51c53ceefa9da3bf85b05280d
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-11-03 19:17:15 -0300

    fix: add thumbnail for tests

--- functions/__tests__/features/Product/fixtures.ts
+++ functions/__tests__/features/Product/fixtures.ts
@@ -2,6 +2,7 @@ import { Product } from "../../../src/model/Product";
 
 export const product: Product = {
   name: "product",
+  thumbnail: "https://cdn-cosmos.bluesoft.com.br/products/7896100501829",
   eanCode: "eanCode",
   ncmCode: "ncmCode",
   unity: { name: "un" },
