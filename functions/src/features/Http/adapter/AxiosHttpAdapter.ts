76ba61a0bdbf4a1a0cf4727ec35d7978d6e726ce
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-11-03 19:18:23 -0300

    feat: add getBuffer to HttpAdapter

--- functions/src/features/Http/adapter/AxiosHttpAdapter.ts
+++ functions/src/features/Http/adapter/AxiosHttpAdapter.ts
@@ -10,6 +10,25 @@ export class AxiosHttpAdapter implements HttpAdapter {
   constructor(axiosInstance: AxiosInstance) {
     this.axiosInstance = axiosInstance;
   }
+  getBuffer = async (
+    url: string,
+    httpRequest?: HttpRequest
+  ): Promise<HttpResponse> => {
+    try {
+      const config: AxiosRequestConfig = { responseType: "arraybuffer" };
+      if (httpRequest) {
+        config.headers = httpRequest.headers;
+      }
+      const axiosResponse = await this.axiosInstance.get(url, config);
+      const returnData: HttpResponse = {
+        status: axiosResponse.status,
+        body: axiosResponse.data,
+      };
+      return returnData;
+    } catch (error) {
+      throw errorToApplicationException(error, HttpAdapterException);
+    }
+  };
 
   get = async (
     url: string,
--- functions/src/features/Http/adapter/HttpAdapter.ts
+++ functions/src/features/Http/adapter/HttpAdapter.ts
@@ -2,4 +2,5 @@ import { HttpResponse, HttpRequest } from "../../../core/HttpProtocol";
 
 export interface HttpAdapter {
   get: (url: string, httpRequest?: HttpRequest) => Promise<HttpResponse>;
+  getBuffer: (url: string, httpRequest?: HttpRequest) => Promise<HttpResponse>;
 }
