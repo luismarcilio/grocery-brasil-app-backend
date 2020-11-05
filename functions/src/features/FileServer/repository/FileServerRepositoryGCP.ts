b114f189da56d285cfc0150d526dd849a5b2c514
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-11-05 09:47:48 -0300

    feat: FileServerRepositoryGCP

---
+++ functions/src/features/FileServer/repository/FileServerRepositoryGCP.ts
@@ -0,0 +1,37 @@
+import { FileServerRepository } from "./FileServerRepository";
+import { Storage } from "@google-cloud/storage";
+
+export class FileServerRepositoryGCP implements FileServerRepository {
+  private readonly storage: Storage;
+
+  constructor(storage: Storage) {
+    this.storage = storage;
+  }
+
+  save = async (
+    file: Buffer,
+    bucket: string,
+    fileName: string,
+    contentType: string
+  ): Promise<string> => {
+    const p = new Promise<string>((resolve, reject) => {
+      const writeStream = this.storage
+        .bucket(bucket)
+        .file(fileName)
+        .createWriteStream({
+          gzip: true,
+          public: true,
+          resumable: false,
+          metadata: {
+            contentType,
+          },
+        });
+      writeStream.on("error", reject).on("finish", resolve).end(file);
+    });
+
+    await p;
+    return Promise.resolve(
+      `https://storage.googleapis.com/${bucket}/${fileName}`
+    );
+  };
+}
