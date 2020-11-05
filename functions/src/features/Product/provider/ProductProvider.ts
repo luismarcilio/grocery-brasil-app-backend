3ba79b5cef2594a43b7ba6d8fa452a51f25a4832
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-11-05 11:32:23 -0300

    refactor: implement ISP in  ProductProvider

--- functions/src/features/Product/provider/ProductProvider.ts
+++ functions/src/features/Product/provider/ProductProvider.ts
@@ -1,10 +1,23 @@
 import { Product, ProductPurchase } from "../../../model/Product";
 
-export interface ProductProvider {
+export interface ProductProviderUploadSearchEngine {
+  uploadToSearchEngine: (product: Product) => Promise<Product>;
+}
+
+export interface ProductProviderUpdateProduct {
+  updateProduct: (product: Product) => Promise<Product>;
+}
+
+export interface ProductProviderNormalizeProduct {
+  normalizeProduct: (product: Product) => Promise<Product>;
+}
+
+export interface ProductProvider
+  extends ProductProviderUploadSearchEngine,
+    ProductProviderUpdateProduct,
+    ProductProviderNormalizeProduct {
   save: (product: Product) => Promise<void>;
   saveNf: (product: Product, productPurchase: ProductPurchase) => Promise<void>;
   getProductById: (docId: string) => Promise<Product>;
   getDocId: (product: Product) => string;
-  updateProduct: (product: Product) => Promise<Product>;
-  normalizeProduct: (product: Product) => Promise<Product>;
 }
