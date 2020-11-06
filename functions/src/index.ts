d05758171a29ef23f6bdf4875d08d235711a6243
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-11-06 14:31:50 -0300

    feat: index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,33 +1,3 @@
-import * as functions from 'firebase-functions'
-import * as express from 'express'
-import * as cors from 'cors'
-import { getGeolocationApiKey } from './functions/getGeolocationApiKey'
-import { getAutocompleteByPhrasePrefix } from './functions/getAutocompleteByPhrasePrefix'
-import { initializeFirebase, authenticate } from './business/expressMiddleware'
-import { parseAndSaveNf } from './functions/parseAndSaveNf'
-import { getNfDataByInitialUrl } from './functions/getNfDataByInitialUrl'
+import * as setup from "./googleCloud/setup";
 
-const app = express()
-app.use(express.json())
-app.use(cors({ origin: true }))
-app.use(authenticate)
-app.use(initializeFirebase)
-app.get('/geolocationApiKey', getGeolocationApiKey)
-app.get('/autocomplete/:phrase', getAutocompleteByPhrasePrefix)
-app.post('/parseAndSaveNf/:state', parseAndSaveNf)
-app.get('/nfDataByInitialUrl', getNfDataByInitialUrl)
-
-// app.get('*', function (req, res) {
-//     res.status(404).send('Not Found');
-// });
-
-// app.post('*', function (req, res) {
-//     res.status(404).send('Not Found');
-// });
-
-// http functions
-exports.v1 = functions.https.onRequest(app)
-
-// Firestore functions
-exports.uploadToElasticsearch = require('./functions/uploadToElasticsearch')
-exports.uploadThumbnail = require('./functions/uploadThumbnail')
+exports = setup;
