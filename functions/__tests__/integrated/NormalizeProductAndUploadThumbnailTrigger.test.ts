64c75f66a253dd2e9af7c7d4bb34169388377a9f
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-11-09 17:18:05 -0300

    test: NormalizeProductAndUploadThumbnailTrigger

---
+++ functions/__tests__/integrated/NormalizeProductAndUploadThumbnailTrigger.test.ts
@@ -0,0 +1,22 @@
+import { makeNormalizeProductAndUploadThumbnailTrigger } from "../../src/factories";
+import { Product } from "../../src/model/Product";
+
+describe("NormalizeProductAndUploadThumbnailTrigger", () => {
+  it("should upload to test search engine", async () => {
+    const sut = makeNormalizeProductAndUploadThumbnailTrigger();
+    const product: Product = {
+      eanCode: "7891962051338",
+      name: "PAO DE FORMA TRADICIONAL 400G VISCONTI",
+      ncmCode: "19059010",
+      unity: { name: "PT" },
+    };
+    const expected = { ...product };
+    expected.normalized = true;
+    expected.thumbnail =
+      "https://storage.googleapis.com/grocery-brasil-app-thumbnails/7891962051338";
+
+    const actual = await sut.call(product);
+    expect(actual).toEqual(expected);
+    console.log(actual);
+  });
+});
