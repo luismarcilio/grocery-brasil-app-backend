932d219fd253d5495e4667896f5d0ae56c005f1c
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-11-09 15:56:38 -0300

    fix: increase json parser body limit

--- functions/src/presentation/express/app.ts
+++ functions/src/presentation/express/app.ts
@@ -11,7 +11,7 @@ import { expressRouteAdapter } from "./expressRouteAdapter";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cors({ origin: true }));
 app.use(expressMiddlewareAdapter(makeAuthenticationMiddleware()));
 
