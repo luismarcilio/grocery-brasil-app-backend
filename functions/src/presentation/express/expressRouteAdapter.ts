c67bbaf7c7c49078d169fd6c191f9ce7b44d829d
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-11-09 10:30:56 -0300

    fix: add query string params

--- functions/src/presentation/express/expressRouteAdapter.ts
+++ functions/src/presentation/express/expressRouteAdapter.ts
@@ -2,12 +2,19 @@ import { Request, Response } from "express";
 import { Controller } from "../controllers/Controller";
 import { HttpRequest } from "../../core/HttpProtocol";
 export const expressRouteAdapter = (controller: Controller) => {
-  return  async (request: Request, response: Response):Promise<void> => {
+  return async (request: Request, response: Response): Promise<void> => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const params: any = {};
+    for (const k in request.query) {
+      params[k] = request.query[k];
+    }
+    for (const k in request.params) {
+      params[k] = request.params[k];
+    }
     const httpRequest: HttpRequest = {
-      params: request.params,
+      params,
       body: request.body,
     };
-
     const httpResponse = await controller.handle(httpRequest);
     response.status(httpResponse.status).send(httpResponse.body);
   };
