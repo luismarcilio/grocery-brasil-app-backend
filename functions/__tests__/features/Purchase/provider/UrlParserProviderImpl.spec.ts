02ad9bc7d35ecf7b1ee7552c72ea18fef05ce651
Luis Marcilio Braga <lbraga@pavlovmedia.com>
2021-05-12 08:42:51 -0300

    feat: UrlParserProviderImpl for state of SP

--- functions/__tests__/features/Purchase/provider/UrlParserProviderImpl.spec.ts
+++ functions/__tests__/features/Purchase/provider/UrlParserProviderImpl.spec.ts
@@ -22,6 +22,12 @@ describe("UrlParserProviderImpl", () => {
       "RJ",
       "33200561585865044442650060001312301776030906",
     ],
+    [
+      "CFe35210509060964011720590007833501406811251697|20210502153116|66.02|96515104634|fFAG+RkGqQuIFyzsBUWHnGdjzkOKuCm2H3bneZbG8amtZ+3bp+S4+HCPdexUFSZz8gKGILIHX9lIxGzvgXP0gMz1rw4xA2RLZ80ppI3Zogwc8nqcT6M1kex0ziNB4gmrZjyQ/SOB3r7YeATVd/zvO0snfVVos6cVxGx/TibyjcU0ngX0B5ENNEEuvvN+Lsfh8jn1wR4KKS9yCBJ5FzAkpNfKtO+7I6dsIte9ZtnFSP/5l2fbXIqajDhFdNNhP/fp6BfT+XGzksttgo2uxEHsqam5ioUpUhLMplfZULOtvWoCNO1nKZppJy/gpYLLiPIY35SUQ4wuFaB2sOoUZt6wIA==",
+      "SP",
+      "35210509060964011720590007833501406811251697"
+    ]
+
   ]).it(
     "should parse the url and return state and accessKey",
     (url, expectedUF, expectedAccessKey) => {
--- functions/src/features/Purchase/provider/UrlParserProviderImpl.ts
+++ functions/src/features/Purchase/provider/UrlParserProviderImpl.ts
@@ -8,25 +8,35 @@ import { withLog, loggerLevel } from "../../../core/Logging";
 export class UrlParserProviderImpl implements UrlParserProvider {
   @withLog(loggerLevel.DEBUG)
   parseURL(url: string): { uf: string; accessKey: string } {
-    const uri: URL = new URL(url);
+    try {
+      const uri: URL = new URL(url);
 
-    if (!uri.hostname.match(/..\.gov\.br/)) {
-      throw new PurchaseException({
-        messageId: MessageIds.INVALID_ARGUMENT,
-        message: `invalid hostname for url: ${url}`,
-      });
-    }
-    const searchParams = uri.searchParams.get("p");
-    if (!searchParams) {
-      throw new PurchaseException({
-        messageId: MessageIds.INVALID_ARGUMENT,
-        message: `invalid queryString for url: ${url}`,
-      });
-    }
+      if (!uri.hostname.match(/..\.gov\.br/)) {
+        throw new PurchaseException({
+          messageId: MessageIds.INVALID_ARGUMENT,
+          message: `invalid hostname for url: ${url}`,
+        });
+      }
+      const searchParams = uri.searchParams.get("p");
+      if (!searchParams) {
+        throw new PurchaseException({
+          messageId: MessageIds.INVALID_ARGUMENT,
+          message: `invalid queryString for url: ${url}`,
+        });
+      }
 
-    const splitUf = uri.hostname.split(".");
-    const uf = splitUf[splitUf.length - 3].toUpperCase();
-    const accessKey = searchParams.split("|")[0];
+      const splitUf = uri.hostname.split(".");
+      const uf = splitUf[splitUf.length - 3].toUpperCase();
+      const accessKey = searchParams.split("|")[0];
+      return { uf, accessKey };
+    } catch (e) {
+      return this.parseNonURL(url);
+    }
+  }
+  parseNonURL(url: string): { uf: string; accessKey: string } {
+    // The only non url implemented so far is SP.
+    const uf = "SP";
+    const accessKey = url.split("|")[0].substr(3);
     return { uf, accessKey };
   }
 }
