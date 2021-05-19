e9678f832f3629f542132f8f10450aab981d2ca5
Luis Marcilio Braga <lbraga@pavlovmedia.com>
2021-05-19 16:15:03 -0300

    feat: integrated test for RJ invoice

--- functions/__tests__/integrated/ParseAndSaveNF.test.ts
+++ functions/__tests__/integrated/ParseAndSaveNF.test.ts
@@ -5,11 +5,12 @@ import { promisify } from "util";
 import * as admin from "firebase-admin";
 
 describe("Express Functions", () => {
-  const htmlFile =
-    "__tests__/features/Purchase/service/fixtures/valid_nf_from_mg.xhtml";
   const readFilePromise = promisify(fs.readFile);
+  
 
   it("should save the NF from MG", async () => {
+    const htmlFile =
+      "__tests__/features/Purchase/service/fixtures/valid_nf_from_mg.xhtml";
     jest.setTimeout(30000);
     const html = (await readFilePromise(htmlFile)).toString();
     const userId = "testUserId";
@@ -38,4 +39,37 @@ describe("Express Functions", () => {
     expect(result.status).toEqual(200);
     expect(result.body).toEqual({ status: "SUCCESS" });
   });
+
+  it("should save the NF from RJ", async () => {
+    const htmlFile =
+      "__tests__/features/Purchase/service/fixtures/valid_nf_from_rj.xhtml";
+    jest.setTimeout(30000);
+    const html = (await readFilePromise(htmlFile)).toString();
+    const userId = "testUserId";
+    const accessKey = "33200561585865044442650060001312301776030906";
+    const firestore = admin.firestore();
+
+    await firestore
+      .collection("COMPRAS")
+      .doc(userId)
+      .collection("COMPLETA")
+      .doc(accessKey)
+      .delete();
+    await firestore
+      .collection("COMPRAS")
+      .doc(userId)
+      .collection("RESUMIDA")
+      .doc(accessKey)
+      .delete();
+    await firestore.collection("COMPRAS").doc(userId).delete();
+
+    const path = "/parseAndSaveNf/RJ";
+    const result = await request(app)
+      .post(path)
+      .send({ html })
+      .set("Authorization", "Bearer " + "jwt");
+    expect(result.status).toEqual(200);
+    expect(result.body).toEqual({ status: "SUCCESS" });
+  });
+
 });
